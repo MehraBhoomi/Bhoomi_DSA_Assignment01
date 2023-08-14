c5c37192838d4a85784f8fc3a5adfac9ab8e8663
MehraBhoomi <137167780+MehraBhoomi@users.noreply.github.com>
2023-08-14 18:48:37 +0530

    Add files via upload

---
+++ Ques05.py
@@ -0,0 +1,14 @@
+# Question 05] Read about the Tower of Hanoi algorithm. Write a program to implement it.
+
+
+def tower_of_hanoi(n, source, auxiliary, target):
+    if n == 1:
+        print(f"Move disk 1 from {source} to {target}")
+        return
+    tower_of_hanoi(n - 1, source, target, auxiliary)
+    print(f"Move disk {n} from {source} to {target}")
+    tower_of_hanoi(n - 1, auxiliary, source, target)
+
+# Example usage
+num_disks = 3
+tower_of_hanoi(num_disks, 'A', 'B', 'C')
